0eac60decaf026328bca851e75618b561b6e2be6
Nicolas Castro <nicolas.y0909@gmail.com>
2025-02-16 17:00:44 -0300

    refactor(src/content/service/content.service): remoção da validação se há tipo, pois baseado nas regras de negócio é obrigatório o tipo no banco.

--- src/content/service/content.service.ts
+++ src/content/service/content.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common'
+import { Inject, Injectable, Logger } from '@nestjs/common'
 import { ProvisionDto } from 'src/content/dto'
 import { ProvisionerFactory } from '@/content/factory'
 import { ContentRepository } from '@/content/providers/repository'
@@ -17,11 +17,6 @@ export class ContentService implements ContentServiceContract {
   async provision(contentId: string): Promise<ProvisionDto> {
     this.logger.log(`Provisioning content for id=${contentId}`)
     const content = await this.contentRepository.findOne(contentId)
-
-    if (!content.getType()) {
-      this.logger.warn(`Missing content type for ID=${contentId}`)
-      throw new BadRequestException('Content type is missing')
-    }
 
     const provisioner = ProvisionerFactory.getByContent(content)
     const provisionedContent = provisioner.provision()
