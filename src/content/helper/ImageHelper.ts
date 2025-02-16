06c5451aaad5806fc9d532df052a9eee9fa21c6f
Nicolas Castro <nicolas.y0909@gmail.com>
2025-02-16 16:59:17 -0300

    refactor(src/content/helper/ImageHelper): correção da importação do pacote path do node.

--- src/content/helper/ImageHelper.ts
+++ src/content/helper/ImageHelper.ts
@@ -1,7 +1,7 @@
-import path from 'node:path'
+import * as path from 'node:path'
 
 export class ImageHelper {
   static getFormatFromUrl(url: string): string {
-    return path.extname(url || '').slice(1) || 'jpg'
+    return path.extname(url || '')?.slice(1) || 'jpg'
   }
 }
