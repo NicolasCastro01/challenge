c0ee09a9ed628a9ef1a58dfaf0ca0a8718bff3bf
Nicolas Castro <nicolas.y0909@gmail.com>
2025-02-16 00:44:22 -0300

    refactor(src/content/resolver/content.resolver): validando a requisição direto no resolvedor e utilizando injeção de dependência.

--- src/content/resolver/content.resolver.ts
+++ src/content/resolver/content.resolver.ts
@@ -1,18 +1,27 @@
-import { Logger, UseGuards } from '@nestjs/common'
+import { Inject, Logger, UnprocessableEntityException, UseGuards } from '@nestjs/common'
 import { Resolver, Args, Context, Query } from '@nestjs/graphql'
-import { ContentService } from 'src/content/service'
-import { ProvisionDto } from 'src/content/dto'
-import { AuthGuard } from 'src/user/guard'
+import { ProvisionDto } from '@/content/dto'
+import { AuthGuard } from '@/user/guard'
+import { ContentServiceContract as ContentServiceProvider } from '@/content/providers/service'
+import { ContentServiceContract } from '@/content/contracts/service'
 
 @Resolver()
 export class ContentResolver {
   private readonly logger = new Logger(ContentResolver.name)
 
-  constructor(private readonly contentService: ContentService) {}
+  constructor(
+    @Inject(ContentServiceProvider)
+    private readonly contentService: ContentServiceContract,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Query(() => ProvisionDto)
   provision(@Args('content_id') contentId: string, @Context('req') req): Promise<ProvisionDto> {
+    if (!contentId) {
+      this.logger.error(`Invalid Content ID: ${contentId}`)
+      throw new UnprocessableEntityException(`Content ID is invalid: ${contentId}`)
+    }
+
     this.logger.log(`Provisioning content=${contentId} to user=${req.user.id}`)
     return this.contentService.provision(contentId)
   }
